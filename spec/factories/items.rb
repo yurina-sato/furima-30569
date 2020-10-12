37c0ced4b9b27a6a31fcd40db68d2fe10f49dc1d
Yurina.Sato <yuplico5221@gmail.com>
2020-10-12 14:52:04 +0900

    Itemモデルの単体テストコード記述

---
+++ spec/factories/items.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :item do
+    name               { Faker::Lorem.word }
+    text               { Faker::Lorem.sentences }
+    price              { Faker::Number.between(from: 300, to: 9999999) }
+    status_id          { Faker::Number.between(from: 1, to: 6) }
+    category_id        { Faker::Number.between(from: 1, to: 10) }
+    prefecture_id      { Faker::Number.between(from: 1, to: 47) }
+    day_id             { Faker::Number.between(from: 1, to: 3) }
+    delivery_charge_id { Faker::Number.between(from: 1, to: 2) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
