57a31c216d5ed85610577acbfaf6a1d87e9566d8
Yurina.Sato <yuplico5221@gmail.com>
2020-10-12 14:49:23 +0900

    itemsコントローラー、ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except:[:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :price, :status_id, :category_id, :prefecture_id, :day_id, :delivery_charge_id).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
-  resources :items, only: [:index]
+  resources :items, only: [:index, :new, :create]
   root to: "items#index"
 end
