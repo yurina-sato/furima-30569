cede5bf4cae1f349a1dc0b003d30965f6cdfad87
Yurina.Sato <yuplico5221@gmail.com>
2020-10-12 14:51:43 +0900

    JavaScriptファイルの作成(手数料・利益計算)

---
+++ app/javascript/price.js
@@ -0,0 +1,17 @@
+function price (){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const priceVal = itemPrice.value; //入力金額を取得
+    const addTaxPrice = document.getElementById("add-tax-price"); //手数料を表示するHTMLを取得
+    const profit = document.getElementById("profit"); //販売利益をを表示するHTMLを取得
+    const tax = 0.1; //消費税を定義
+    const sellTax = Math.floor(priceVal * tax); //販売手数料の計算
+    const sellProfit = priceVal - sellTax; //販売利益の計算
+
+    addTaxPrice.innerHTML = `${sellTax}`;
+    profit.innerHTML = `${sellProfit}`;
+  });
+};
+
+
+window.addEventListener('load', price);
