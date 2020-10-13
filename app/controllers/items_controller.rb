76a8632183b6f4e24801b6a79a4cfe966a81dfdb
Yurina.Sato <yuplico5221@gmail.com>
2020-10-13 14:21:52 +0900

    【Rubocop】調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user.id
   end
 end
