ea2f4ab076aebbe9462ede718421c57a98a7780b
Yurina.Sato <yuplico5221@gmail.com>
2020-10-16 14:24:43 +0900

    OrderAddressモデル修正(バリデーションの重複を解消)

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,10 +8,8 @@ class OrderAddress
     validates :postal_cord, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' } # 郵便番号の正規表現(3桁-4桁)
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
     validates :city, :house_number, :token
-    validates :phone_number, numericality: { with: /\A\d{10,11}\z/, message: ' Input only number' } # 電話番号(ハイフンなし10桁or11桁)
+    validates :phone_number, numericality: { with: /\A\d{10,11}\z/, message: ' Input only number' } # 電話番号(半角・ハイフンなし10桁or11桁)
   end
-  # validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: ' Input only number' } # 半角数字の正規表現
-
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
