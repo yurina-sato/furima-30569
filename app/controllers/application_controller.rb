fe7c6385695f7ce1522cd4c32471eb7cda994f69
Yurina.Sato <yuplico5221@gmail.com>
2020-10-19 23:39:42 +0900

    【Rubocop】調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["FURIMA_BASIC_AUTH_USER"] && password == ENV["FURIMA_BASIC_AUTH_PASSWORD"] 
+      username == ENV['FURIMA_BASIC_AUTH_USER'] && password == ENV['FURIMA_BASIC_AUTH_PASSWORD']
     end
   end
 
