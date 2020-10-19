590816858a01d185dcb01038b746297859f681dd
Yurina.Sato <yuplico5221@gmail.com>
2020-10-19 14:46:24 +0900

    slick スライドのJSファイル記述

---
+++ app/javascript/slick.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<i class="fas fa-angle-left"></i>',
+    nextArrow: '<i class="fas fa-angle-right"></i>'
+
+  });
+});
+
