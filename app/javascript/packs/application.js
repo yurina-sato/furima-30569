cce0390780ba935d58b025dc71567d88f55399ea
Yurina.Sato <yuplico5221@gmail.com>
2020-10-20 19:24:58 +0900

    【gem】bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,10 @@ require("../card")
 require("../preview")
 require("../slick")
 
+//= require jquery3
+//= require popper  
+//= require bootstrap-sprockets  
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
