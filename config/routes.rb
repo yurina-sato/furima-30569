2852811bceda91e01ad175f8a406665db114c173
Yurina.Sato <yuplico5221@gmail.com>
2020-10-21 18:37:46 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items do
     resources :orders, only: [:index, :create]
+    collection do
+      get 'search'
+    end
   end
   root to: "items#index"
 end
