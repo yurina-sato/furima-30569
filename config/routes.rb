c993e0c3d9463256456405f744701866ef13234f
Yurina.Sato <yuplico5221@gmail.com>
2020-10-23 12:39:31 +0900

    ルーティング設定(create, destroyアクション)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create]
     resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
