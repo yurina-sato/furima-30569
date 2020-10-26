9e1cdcf8366aa9b47d9dab517bff92ef984b8e6a
Yurina.Sato <yuplico5221@gmail.com>
2020-10-26 16:34:16 +0900

    deviseのsign_inヘルパー導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+require 'devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +64,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::IntegrationHelpers, type: :request #requwstテストにdeviseのsign_inヘルパー導入
+  config.include FactoryBot::Syntax::Methods #念のためFactoryBotもinclude
+
+  
 end
