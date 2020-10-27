1382af3bc84b93859478ec545f2a6b82453c460f
Yurina.Sato <yuplico5221@gmail.com>
2020-10-27 19:43:31 +0900

    basic認証が通るように記述:commentsコントローラー

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :request do
   describe "POST #create" do
+    def post_new_comment(item_id, params) # basic認証と、一緒に送るパラメータ
+      username = ENV['FURIMA_BASIC_AUTH_USER']
+      password = ENV['FURIMA_BASIC_AUTH_PASSWORD']
+      post item_comments_path(item_id), headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) },
+                             params: { comment: params }
+    end
     context 'ユーザーがログインしている場合' do
       let(:user) { FactoryBot.create(:user) } # ログインするuserを予め作成
 
@@ -14,27 +20,27 @@ RSpec.describe CommentsController, type: :request do
 
       it 'createアクションをリクエストすると、正常にコメントを投稿できる' do
         expect do
-          post item_comments_path(@item.id), params: { comment: @comments_params }
+          post_new_comment(@item.id, @comments_params) 
         end.to change{ Comment.count }.by 1
       end
       it 'createアクションをリクエストすると、正常にコメント投稿ができた場合は商品詳細ページへリダイレクトする' do
-        post item_comments_path(@item.id), params: { comment: @comments_params }
+        post_new_comment(@item.id, @comments_params) 
         expect(response).to redirect_to item_path(@item.id)
       end
       it 'createアクションをリクエストすると、パラメータが不正な場合はコメントを投稿できない' do
         expect do
-          post item_comments_path(@item.id), params: { comment: @invalid_comments_params }
+          post_new_comment(@item.id, @invalid_comments_params) 
         end.to_not change{ Comment.count }
       end
       it 'createアクションをリクエストすると、正常にコメント投稿ができない場合は商品情報ページへ戻る'do
-      post item_comments_path(@item.id), params: { comment: @invalid_comments_params }
+      post_new_comment(@item.id, @invalid_comments_params) 
       expect(response).to render_template 'items/show'
       end
     end
     context 'ユーザーがログアウトしている場合' do
       it 'createアクションにリクエストすると、ログインページへリダイレクトする' do
         @item = FactoryBot.create(:item)
-        post item_orders_path(@item.id), params: { order_address: @order_address_params }
+        post_new_comment(@item.id, @comments_params) 
         expect(response).to redirect_to new_user_session_path
       end
     end
