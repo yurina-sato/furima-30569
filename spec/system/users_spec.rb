59af3f5eb8b31ff9625314c8df2af5a1605f3f76
Yurina.Sato <yuplico5221@gmail.com>
2020-11-17 22:52:43 +0900

    users結合テストコードを記述(ユーザー新規登録ができないとき)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -15,13 +15,10 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
       basic_pass root_path
-      
       # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
-      
       # 新規登録ページへ移動する
       visit new_user_registration_path
-
       # ユーザー情報を入力する
       fill_in 'ニックネーム', with: @user.nickname
       fill_in 'メールアドレス', with: @user.email
@@ -34,18 +31,14 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       select @user.birth_date.strftime("%Y"), from: 'user_birth_date_1i' # 生"年"月日
       select @user.birth_date.strftime("%-m"), from: 'user_birth_date_2i' # 生年"月"日(0を抜いて左寄せ)
       select @user.birth_date.strftime("%-d"), from: 'user_birth_date_3i' # 生年月"日"(0を抜いて左寄せ)
-
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change { User.count }.by(1)
-      
       # トップページへ遷移したことを確認する
       expect(current_path).to eq root_path
-      
       # ログアウトボタンが表示されていることを確認する
       expect(page).to have_content('ログアウト')
-      
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
@@ -54,11 +47,29 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      basic_pass root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: ""
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: ""
+      fill_in 'パスワード(確認)', with: ""
+      fill_in 'first-name', with: ""
+      fill_in 'last-name', with: ""
+      fill_in 'first-name-kana', with: ""
+      fill_in 'last-name-kana', with: ""
+      select "--", from: 'user_birth_date_1i' # 生"年"月日
+      select "--", from: 'user_birth_date_2i' # 生年"月"日(0を抜いて左寄せ)
+      select "--", from: 'user_birth_date_3i' # 生年月"日"(0を抜いて左寄せ)
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
     end
   end
 end
