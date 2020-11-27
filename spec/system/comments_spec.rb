9c9214501190542176aa4dcce0f5c696255c4c4f
Yurina.Sato <yuplico5221@gmail.com>
2020-11-27 16:40:18 +0900

    comments結合テストコード(exampleの記述)

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :system do
+RSpec.describe "コメント投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @item = FactoryBot.create(:item)
+    @comment = FactoryBot.create(:comment, item_id: @item.id) # @itemへのコメント
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'ログインしたユーザーは商品詳細ページでコメント投稿できる' do
+    # ログインする
+    # 商品詳細ページに遷移する
+    # フォームに情報を入力する
+    # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
+    # 詳細ページにリダイレクトされることを確認する
+    # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
+  end
+  it 'ログインしていないと商品詳細ページでコメント投稿ができない' do
+    # トップページに移動する
+    # 商品詳細ページに遷移する
+    # 商品詳細ページにコメント投稿フォームがないことを確認する
+  end
+
 end
