214ed5471c2ced4a9723d6ab60279724a107ed6a
Yurina.Sato <yuplico5221@gmail.com>
2020-11-30 12:22:45 +0900

    likes結合テストコードを記述(お気に入り削除ができない時を追加)

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -51,36 +51,53 @@ RSpec.describe "お気に入り削除", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @like = FactoryBot.create(:like, user_id: @user.id, item_id: @item.id)
+    @like = FactoryBot.create(:like, user_id: @user.id, item_id: @item.id) # @userの@itemへのお気に入り登録
+    @another_user = FactoryBot.create(:user) # 別ユーザー
   end
 
-  it 'ログインしたユーザーはお気に入り登録した商品の詳細ページでお気に入り削除ができる' do
-    # ログインする
-    basic_pass new_user_session_path
-    fill_in 'メールアドレス', with: @user.email
-    fill_in 'パスワード', with: @user.password
-    find('input[name="commit"]').click
-    expect(current_path).to eq root_path
-    # 商品詳細ページに遷移する
-    visit item_path(@item.id)
-    # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
-    expect(page).to have_link 'お気に入りから削除'
-    # お気に入り解除ボタンをクリックすると、Likeモデルのカウントが1下がることを確認する
-    expect{
-      find_link('お気に入りから削除').click
-    }.to change { Like.count }.by(-1)
-    # 「お気に入りから削除しました。」の文字があることを確認する
-    expect(page).to have_content('お気に入りから削除しました。')
-    # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
-    expect(page).to have_content('お気に入り')
+  context 'お気に入り削除ができるとき' do
+    it 'ログインしたユーザーはお気に入り登録した商品の詳細ページでお気に入り削除ができる' do
+      # ログインする
+      basic_pass new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      # 商品詳細ページに遷移する
+      visit item_path(@item.id)
+      # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
+      expect(page).to have_link 'お気に入りから削除'
+      # お気に入り解除ボタンをクリックすると、Likeモデルのカウントが1下がることを確認する
+      expect{
+        find_link('お気に入りから削除').click
+      }.to change { Like.count }.by(-1)
+      # 「お気に入りから削除しました。」の文字があることを確認する
+      expect(page).to have_content('お気に入りから削除しました。')
+      # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
+      expect(page).to have_content('お気に入り')
+    end
   end
-
-  it 'ログインしていないと商品詳細ページでお気に入り削除ができない' do
-    # トップページに移動する
-    basic_pass root_path
-    # 商品詳細ページに遷移する
-    visit item_path(@item.id)
-    # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
-    expect(page).to have_no_link 'お気に入りから削除'
+  
+  context 'お気に入り削除ができないとき' do
+    it 'ログインしていないと商品詳細ページでお気に入り削除ができない' do
+      # トップページに移動する
+      basic_pass root_path
+      # 商品詳細ページに遷移する
+      visit item_path(@item.id)
+      # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
+      expect(page).to have_no_link 'お気に入りから削除'
+    end
+    it 'ログインユーザーは商品詳細ページで他ユーザーのお気に入り削除ができない' do
+      # 別ユーザーでログインする
+      basic_pass new_user_session_path
+      fill_in 'メールアドレス', with: @another_user.email
+      fill_in 'パスワード', with: @another_user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      # 商品詳細ページに遷移する
+      visit item_path(@item.id)
+      # 詳細ページ上にお気に入り登録ボタンが存在することを確認する
+      expect(page).to have_no_link 'お気に入りから削除'
+    end
   end
 end
