c082932c2a402da69755ea8dc4a5661c294a8a0d
Yurina.Sato <yuplico5221@gmail.com>
2020-12-02 19:01:43 +0900

    orders結合テストコードを記述(商品購入ができる時)※仮

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "商品購入", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @order_address = FactoryBot.build(:order_address)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
 
     @sold_item = FactoryBot.create(:item) # 売却済み商品用
     @sold_order = FactoryBot.create(:order, item_id:@sold_item.id) # 売却済みの購入情報
@@ -25,31 +25,39 @@ RSpec.describe "商品購入", type: :system do
   context '商品購入ができるとき'do
     it 'ログインしたユーザーは商品購入ができる' do
       # ログインする
-      # basic_pass new_user_session_path
-      # fill_in 'メールアドレス', with: @user.email
-      # fill_in 'パスワード', with: @user.password
-      # find('input[name="commit"]').click
-      # expect(current_path).to eq root_path
+      basic_pass new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
       # 商品詳細ページに移動する
-      # visit item_path(@item.id)
+      visit item_path(@item.id)
       # 商品詳細ページに遷移する
-      # visit item_orders_path(@item.id)
+      visit item_orders_path(@item.id)
       # フォームに情報を入力する
-      # クレジットの部分
-      # fill_in '郵便番号', with: @order_address.postal_cord
-      # select Prefecture.find(@order_address.prefecture_id).name, from: 'item-prefecture'
-      # fill_in '市町村', with: @order_address.city
-      # fill_in '番地', with: @order_address.house_number
-      # fill_in '建物名', with: @order_address.building
-      # fill_in '電話番号', with: @order_address.phone_number
+      fill_in 'カード情報', with: '4242424242424242'
+      fill_in 'card-exp-month', with: '1'
+      fill_in 'card-exp-year', with: '23'
+      fill_in 'セキュリティコード', with: '123'
+      fill_in '郵便番号', with: @order_address.postal_cord
+      select Prefecture.find(@order_address.prefecture_id).name, from: 'prefecture'
+      fill_in '市区町村', with: @order_address.city
+      fill_in '番地', with: @order_address.house_number
+      fill_in '建物名', with: @order_address.building
+      fill_in '電話番号', with: @order_address.phone_number
       # 送信するとOrderモデルのカウントが1上がることを確認する
+      # binding.pry
+      payjp_test
+      find('input[name="commit"]').click
+      # expect(Order.count).to eq 2
       # expect{
-      #   find('input[name="commit"]').click
       # }.to change { Order.count }.by(1)
+
       # トップページへ遷移したことを確認する
+      # binding.pry
       # expect(current_path).to eq root_path
       # 「購入手続きが完了しました。」の文字があることを確認する
-      # expect(page).to have_content('購入手続きが完了しました。')
+      expect(page).to have_content('購入手続きが完了しました。')
     end
   end
   context '商品購入ができないとき'do
